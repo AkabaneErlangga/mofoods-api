c6f0c70d3f8be388165af91574768ffdaa292316
Akabane-kun <ursawarrior157@gmail.com>
2021-07-26 10:35:43 +0700

    c

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Antrean from './Antrean/Antrean.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Antrean />
+      </header>
+    </div>
+  );
+}
+
+export default App;
